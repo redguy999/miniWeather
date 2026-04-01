007c34cad54ce520a403bf9bf78380d5f812ee66
Kevin Barr <barrkr@dukes.jmu.edu>
2026-04-01 15:33:34 -0400

    gitignore and some scripts

---
+++ cpp/build/scripts/run_exp1.sh
@@ -0,0 +1,29 @@
+module load mpi
+
+export TEST_MPI_COMMAND="salloc -Q -n 1 --gres=gpu mpirun"
+export PARALLEL_NETCDF_ROOT="/shared/common/pnetcdf-1.14.1"
+export LD_LIBRARY_PATH=/shared/common/pnetcdf-1.14.1/lib:$LD_LIBRARY_PATH
+
+cd ~/miniWeather/cpp/build
+
+./cmake_clean.sh
+
+cmake -DCMAKE_CXX_COMPILER=mpic++         \
+      -DCMAKE_C_COMPILER=mpicc            \
+      -DCMAKE_Fortran_COMPILER=mpif90     \
+      -DYAKL_CXX_FLAGS="-Ofast -march=native -mtune=native -DNO_INFORM -I${PARALLEL_NETCDF_ROOT}/include"   \
+      -DLDFLAGS="-L${PARALLEL_NETCDF_ROOT}/lib -lpnetcdf"  \
+      -DNX=256                            \
+      -DNZ=128                            \
+      -DSIM_TIME=1000                     \
+      -DOUT_FREQ=4                        \
+      -DDATA_SPEC=DATA_SPEC_THERMAL       \
+      ..
+
+make -j $(nproc)
+
+
+salloc -Q -n 4 --gres=gpu mpirun ./parallelfor
+
+
+mv output.nc exp1_output.nc
